08ff362c2dfb37d1f077f9fa7893fe50f830ac46
Sandeep Dilip <sandilip@icloud.com>
2021-09-09 20:16:53 -0400

    Go back to old utils.map function

--- nvim/lua/conf/maps.lua
+++ nvim/lua/conf/maps.lua
@@ -1,19 +1,6 @@
--- use WhichKey to seup and document mappings
-local wk = require'which-key'
-
+local map = require'conf.utils'.map
 vim.g.mapleader = [[ ]]
-vim.g.maplocalleader = [[,]]
 
-wk.register {
-	['kj'] = {'<esc>', 'Exit insert mode', mode = 'i'},
-	['<c-space>'] = {'compe#complete()', 'Invoke compe', mode = 'i', expr = true }
-}
-
-wk.register {
-	['<leader>'] = {
-		[';'] = {':', 'Enter command mode', silent = false},
-		e = {'<cmd>Telescope fd<cr>', 'Fuzzy find file to edit', silent = false},
-		b = {'<cmd>Telescope buffers<cr>', 'Fuzzy switch to open buffer', silent = false},
-		['?'] = {'<cmd>Cheatsheet<cr>', 'Open up cheatsheet'}
-	}
-}
+map('i', 'kj', '<esc>')
+map('n', '<leader>;', ':')
+map('n', '<leader>e', '<cmd>Telescope fd<cr>')
