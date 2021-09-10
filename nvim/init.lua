4c80c87fd624f9e6a7f4cc7b0856d2ce7e540f78
Sandeep Dilip <sandilip@icloud.com>
2021-09-09 22:23:36 -0400

    Require the sets and maps first in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,12 @@
--- Load packer.nvim plugins
-require('conf.packer')
-
 -- Load some basic nvim options
 require('conf.sets')
 
 -- Load my keymappings
 require('conf.maps')
 
+-- Load packer.nvim plugins
+require('conf.packer')
+
 -- Set the colorscheme
 require('conf.colors')
+
