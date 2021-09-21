16e8e2613b9366f8c906219c586a1efc90315380
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2021-09-21 15:01:14 -0400

    Add gh colorscheme and circle icons

--- nvim/lua/conf/packer.lua
+++ nvim/lua/conf/packer.lua
@@ -15,14 +15,44 @@ return require('packer').startup(
 		-- telescope
 		use 'nvim-telescope/telescope.nvim'
 
-		-- colorschemes
-		use 'folke/tokyonight.nvim'
+		use {
+			'projekt0n/circles.nvim',
+			requires = {'kyazdani42/nvim-web-devicons'},
+			config = function () require'circles'.setup{} end
+		}
 
 		-- statusline
+		--[[
+		use {
+			'famiu/feline.nvim',
+			config = function () require'feline'.setup{} end
+		}
+		]]--
+
 		use {
 			'hoob3rt/lualine.nvim',
-			requires = {'kyazdani42/nvim-web-devicons'},
-			config = require'plugins.lualine'
+			requires = {'projekt0n/circles.nvim'},
+			config = function ()
+				require'lualine'.setup {
+					options = {
+						theme = 'github'
+					},
+					sections = {
+						lualine_x = {'encoding', 'filetype'}
+					}
+				}
+			end
+		}
+
+		-- colorschemes
+		use 'folke/tokyonight.nvim'
+		use {
+			'projekt0n/github-nvim-theme',
+			config = function ()
+				require'github-theme'.setup {
+					theme_style = 'dark_default'
+				}
+			end
 		}
 
 		-- treeshitter
@@ -30,22 +60,30 @@ return require('packer').startup(
 			'nvim-treesitter/nvim-treesitter',
 			branch = '0.5-compat',
 			run = ':TSUpdate',
-			config = require'plugins.treesitter'
+			config = function ()
+				require'nvim-treesitter.configs'.setup {
+					ensure_installed = 'maintained',
+					highlight = {
+						enable = true
+					},
+					indent = {
+						enable = false
+					}
+				}
+			end
 		}
 
 		-- load lspconfig
 		use {
 			'neovim/nvim-lspconfig',
-			-- config = function () require'plugins.lsp' end
+			config = function () require'plugins.lsp' end
 		}
 
-		-- load autocomplete
+		-- load autocomplete and snippets
 		use {
 			'ms-jpq/coq_nvim',
 			branch = 'coq',
 		}
-
-		-- snippets to go along with autocomplete
 		use {
 			'ms-jpq/coq.artifacts',
 			branch = 'artifacts'
