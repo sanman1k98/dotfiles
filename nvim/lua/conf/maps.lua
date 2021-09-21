5d9ce40c8b9d2c46ece3444fdc32da4ea70915ab
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2021-09-21 18:42:11 -0400

    Move coq maps from autopairs setup

--- nvim/lua/conf/maps.lua
+++ nvim/lua/conf/maps.lua
@@ -3,4 +3,14 @@ vim.g.mapleader = [[ ]]
 
 map('i', 'kj', '<esc>')
 map('n', '<leader>;', ':')
-map('n', '<leader>e', '<cmd>Telescope find_files<cr>')
+map('n', '<leader>e', '<cmd>Telescope fd<cr>')
+map('n', '<leader>b', '<cmd>Telescope buffers<cr>')
+map('n', '<leader>c', '<cmd>Telescope commands<cr>')
+map('n', '<leader>t', '<cmd>NvimTreeToggle<cr>')
+
+-- coq recommended mappings
+vim.g.coq_settings = { keymap = { recommended = false } }
+map('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true })
+map('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true })
+map('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true })
+map('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true })
