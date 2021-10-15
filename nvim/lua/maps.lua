75f742bb18a95ca5d89d95e721decb1d3ed25fe0
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2021-10-15 13:50:26 -0400

    Unextract utils map function

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,5 +1,12 @@
-local map = require'conf.utils'.map
 vim.g.mapleader = [[ ]]
+local set_keymap = vim.api.nvim_set_keymap
+local function map(modes, lhs, rhs, args)
+  args = args or {}
+	-- assign args.noremap to 'true' if it is not already defined
+  args.noremap = (args.noremap == nil and true) or args.noremap
+  if type(modes) == 'string' then modes = {modes} end
+  for _, mode in ipairs(modes) do set_keymap(mode, lhs, rhs, args) end
+end
 
 map('i', 'kj', '<esc>')
 map('n', '<leader>;', ':')
