8baab2d76d47b18f312ddda8ed532eb0ad15e921
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-07-27 11:57:25 -0400

    WIP: rename "maps" to "mappings"

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require('options')
 
+require('mappings')
 
 require('plugins')
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -36,17 +36,5 @@ map('n', '<leader>c', '<cmd>lua require"telescope.builtin".commands()<cr>')
 -- file tree
 map('n', '<leader>ft', '<cmd>NvimTreeFocus<cr>')
 
--- file browser
-map('n', '<leader>fb', function ()
-	require'telescope'.extensions.file_browser.file_browser()
-end)
-
 -- toggle diagnostics window
 map('n', '<leader>vd', '<cmd>TroubleToggle lsp_workspace_diagnostics<cr>', { silent = true })
-
--- coq recommended mappings
-vim.g.coq_settings = { keymap = { recommended = false } }
-map('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true })
-map('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true })
-map('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true })
-map('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true })
