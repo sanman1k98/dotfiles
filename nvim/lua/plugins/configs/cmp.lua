6e2d02c3a2f913791581dd354db4583d3eca0305
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-07-27 12:21:36 -0400

    WIP: add config for `cmp`

---
+++ nvim/lua/plugins/configs/cmp.lua
@@ -0,0 +1,20 @@
+local present1, cmp = pcall(require, 'cmp')
+local present2, luasnip = pcall(require, 'luasnip')
+
+if not (present1 or present2) then
+	return
+end
+
+cmp.setup {
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	sources = cmp.config.sources {
+		{name = 'nvim_lsp'},
+		{name = 'luasnip'},
+		{name = 'buffer'},
+		{name = 'path'}
+	}
+}
