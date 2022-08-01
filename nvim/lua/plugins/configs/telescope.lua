7bdaff025cf73ab1db3ff55aa36d30f8aa91638b
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-01 18:35:05 -0400

    Remove configuration for `lsp_code_actions` picker
    This picker was removed from telescope; see issue #1470 or
    ":h telescope.changelog-1866"

--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -1,7 +1,7 @@
 local present, telescope = pcall(require, 'telescope')
 
-if not present then 
-	return 
+if not present then
+	return
 end
 
 telescope.setup {
@@ -10,8 +10,5 @@ telescope.setup {
 			find_command = {'fd', '--type', 'file', '--exclude', '{.git,node_modules}'},
 			hidden = true
 		},
-		lsp_code_actions = {
-			theme = 'cursor'
-		}
 	}
 }
