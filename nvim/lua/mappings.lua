cd0918611d67b21f765b928869ca0c4d9929b7ca
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-01 18:28:10 -0400

    Use custom function for all my mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,58 +1,45 @@
 vim.g.mapleader = [[ ]]
-local map = vim.keymap.set
+local map = require'utils.map'	-- custom map function
 
--- better escape
--- map('i', 'kj', '<esc>')
-
--- I don't like pressing shift with my weak left pinky:(
-map('n', '<leader>;', ':')
-
--- search through help tags and open in a new window
-map('n', '<leader>h', function()
-	require'telescope.builtin'.help_tags()
-end)
-
--- write file
-map('n', '<leader>w', '<cmd>w<cr>')
-
--- format selected
-map('v', 'gw', '<esc><cmd>lua vim.lsp.buf.range_formatting()<cr>')
-
--- vim lsp code actions
-map('n', '<leader>va', function ()
-	require'telescope.builtin'.lsp_code_actions()
-end)
-
-map('n', '<leader>vr', function()
-	vim.lsp.buf.rename()
-end)
-
--- fuzzy find files
-map("n", "<leader>ff", function ()
-	require'telescope.builtin'.find_files()
-end)
-
--- buffers
-map('n', '<leader>fb', function()
-	require'telescope.builtin'.buffers()
-end)
-
--- search through commands
-map('n', '<leader>c', function()
-	require'telescope.builtin'.commands()
-end)
-
--- file tree
-map('n', '<leader>ft', '<cmd>NvimTreeFocus<cr>')
-
--- custom map function
-
-require'utils.map' {
+map {
 	i = {
 		['kj'] = {
 			'<esc>',
 			'quickly leave insert mode'
 		}
+	},
+
+	n = {
+		['<leader>;'] = {
+			':',
+			'easily enter a command'
+		},
+
+		['<leader>h'] = {
+			function() require'telescope.builtin'.help_tags() end,
+			'search help docs'
+		},
+
+		['<leader>w'] = {
+			'<cmd> w <cr>',
+			'write file'
+		},
+
+		['<leader>vr'] = {
+			function() vim.lsp.rename() end,
+			'rename a symbol'
+		},
+
+		['<leader>ff'] = {
+			function() require'telescope.builtin'.find_files() end,
+			'fuzzy find files'
+		},
+
+		['<leader>ft'] = {
+			function()
+				require'nvim-tree.api'.tree.focus()
+			end,
+			'focus file tree'
+		},
 	}
 }
-
