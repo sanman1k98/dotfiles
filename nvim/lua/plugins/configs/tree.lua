3352be39a8574f68f1b1ad372955e12e08176163
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-02 00:30:10 -0400

    Change `nvim-tree` options
    - quit on file open
    - change default width
    - no `special_files`

--- nvim/lua/plugins/configs/tree.lua
+++ nvim/lua/plugins/configs/tree.lua
@@ -6,16 +6,22 @@ end
 
 tree.setup {
 	view = {
+		width = 25,
 		hide_root_folder = true,
 		adaptive_size = true
 	},
+
 	renderer = {
-		indent_markers = {
-			enable = true
-		},
 		icons = {
 			git_placement = "after"
 		},
-		group_empty = true
+		group_empty = true,
+		special_files = {}
+	},
+
+	actions = {
+		open_file = {
+			quit_on_open = true
+		}
 	}
 }
