303d8fcc9706f955a9194cdaa3e2eb8fa55c2f5e
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-02 00:22:47 -0400

    Add `defaults` to telescope setup func
    Inspired by NvChad.

--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -5,6 +5,27 @@ if not present then
 end
 
 telescope.setup {
+	defaults = {
+		prompt_prefix = '   ',
+		selection_caret = '  ',
+		entry_prefix = '  ',
+		sorting_strategy = 'ascending',
+		layout_config = {
+			horizontal = {
+				prompt_position = 'top',
+				preview_width = 0.55,
+				results_width = 0.8,
+			},
+			vertical = {
+				mirror = false
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
+		},
+		border = false -- TODO: set highlights to get that borderless look
+	},
+
 	pickers = {
 		find_files = {
 			find_command = {'fd', '--type', 'file', '--exclude', '{.git,node_modules}'},
