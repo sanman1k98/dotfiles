00a98ae0cb4edd19b7bc3262c8ea489e29eabda3
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-17 18:01:42 -0400

    Rewrite init.lua
    Since the options and mappings are in the `plugin/` directory which gets
    sourced automatically, we only need to `require` the plugins module.
    
    Also add an autocommand which sources the plugins module when it
    changes.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,13 @@
-require('options')
 
-require('mappings')
+local au = vim.api.nvim_create_autocmd
 
-require('plugins')
+do -- plugins
+  require "plugins"
+  au("BufWritePost", {
+    pattern = vim.fn.stdpath("config") .. "/lua/plugins.lua",
+    callback = function()
+      vim.cmd.source "<amatch>"
+    end
+  })
+end
+
