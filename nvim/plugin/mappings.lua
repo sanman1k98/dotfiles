35261915876fe392287a352af4e21b3c0f354a5a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-17 17:13:51 -0400

    Use double quotes

--- nvim/plugin/mappings.lua
+++ nvim/plugin/mappings.lua
@@ -1,61 +1,61 @@
 vim.g.mapleader = [[ ]]
-local map = require'utils.map'	-- custom map function
+local map = require("utils.map")	-- custom map function
 
 map {
 	i = {
-		['kj'] = {
-			'<esc>',
-			'quickly leave insert mode'
+		["kj"] = {
+			"<esc>",
+			"quickly leave insert mode"
 		}
 	},
 
 	n = {
-		['<leader>;'] = {
-			':',
-			'easily enter a command'
+		["<leader>;"] = {
+			":",
+			"easily enter a command"
 		},
 
-		['<leader>h'] = {
-			function() require'telescope.builtin'.help_tags() end,
-			'search help docs'
+		["<leader>h"] = {
+      function() require("telescope.builtin").help_tags() end,
+			"search help docs"
 		},
 
-		['<leader>w'] = {
-			'<cmd> w <cr>',
-			'write file'
+		["<leader>w"] = {
+			"<cmd> w <cr>",
+			"write file"
 		},
 
-		['<leader>ff'] = {
-			function() require'telescope.builtin'.find_files() end,
-			'fuzzy find files'
+		["<leader>ff"] = {
+			function() require"telescope.builtin".find_files() end,
+			"fuzzy find files"
 		},
 
-		['<leader>c'] = {
-			function() require'telescope.builtin'.commands() end,
-			'search for a command'
+		["<leader>c"] = {
+      function() require("telescope.builtin").commands() end,
+			"search for a command"
 		},
 
-		['<leader>ft'] = {
+		["<leader>ft"] = {
 			function()
-				require'nvim-tree.api'.tree.focus()
+				require("nvim-tree.api").tree.focus()
 			end,
-			'focus file tree'
+			"focus file tree"
 		},
 
-		['<leader>ng'] = {
+		["<leader>ng"] = {
 			function()
-				require'neogit'.open()
+        require("neogit").open()
 			end,
-			'open neogit'
+			"open neogit"
 		}
 	},
 
   v = {
-    ['<leader>so'] = {
+    ["<leader>so"] = {
       function ()
-        vim.notify('TODO: implement this function', vim.log.levels.INFO)
+        vim.notify("TODO: implement this function", vim.log.levels.INFO)
       end,
-      '":source" visual selection'
+      "':source' visual selection"
     }
   }
 }
