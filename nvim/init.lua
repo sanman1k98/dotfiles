c9ca3e93d84f458257792f46131f6d2664f92b9e
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-28 10:12:22 -0400

    Refactor `init.lua`
    Require `keymaps` and `options`.
    
    Add autocmds to automatically apply new changes to `keymaps` and
    `options` files.
    
    Check `loadplugins` option before attempting to bootstrap Paq.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,42 @@
-
+-- false if nvim was started with "--noplugin"
+local load_plugins = vim.opt.loadplugins:get()
+local cmd = vim.cmd
 local au = vim.api.nvim_create_autocmd
+-- create augroups and return a table containing their names and IDs
+local augroups = require "augroups"
 
-do -- plugins
-  require "plugins"
+
+
+do -- main configuration
+  require "options"
+  require "keymaps"
+  -- source matching file in "~/.config/nvim/colors/"
+  cmd.colorscheme "catppuccin"
+end
+
+
+do -- automatically apply changes without restarting nvim
+  local lua_dir = vim.fn.stdpath("config") .. "/lua"
+  local paths = {
+    lua_dir .. "/keymaps.lua",
+    lua_dir .. "/options.lua",
+  }
   au("BufWritePost", {
-    pattern = vim.fn.stdpath("config") .. "/lua/plugins.lua",
-    callback = function()
-      vim.cmd.source "<amatch>"
+    group = augroups.user_config_reloader,
+    pattern = paths,
+    callback = function(tb)
+      local file_path = tb.match
+      cmd.source(file_path)
     end
   })
 end
 
-do -- colors
-  vim.cmd.colorscheme "catppuccin"
+
+do -- load plugin manager
+  local paq_loaded = pcall(require, "paq")
+  -- install if nvim is going to load plugins
+  if load_plugins and not paq_loaded then
+    local bootstrap = require "utils.bootstrap"
+    bootstrap.add_paq()   -- adds to packpath
+  end
 end
