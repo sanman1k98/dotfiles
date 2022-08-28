e1e43ade1ea1d65a2bd56e2bbd234f5c3fd5f34f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-28 10:05:32 -0400

    Use `options` as module
    Same as last commit: move from `plugin` dir to `lua` dir.
    
    Some refactoring as well; wrap related options in `do` blocks. Make
    option setting for string-like options more consistent.
    
    Add autocmds to highlight on yank.

---
+++ nvim/lua/options.lua
@@ -0,0 +1,125 @@
+local augroups = require "augroups"
+local au = vim.api.nvim_create_autocmd
+local env = vim.fn.env
+local opt = vim.opt
+
+
+
+do -- UI
+  if env.TERM == "xterm-kitty" or env.COLORTERM == "truecolor" then
+    opt.termguicolors = true
+  end
+
+  opt.helpheight = 12
+  opt.splitright = true
+  opt.splitbelow = true
+
+  opt.pumheight = 30        -- max number of items to show in the popup menu
+
+  opt.title = true          -- set window title to "titlestring"
+  opt.showtabline = 1       -- show tabline for more than one tab
+  opt.laststatus = 3        -- global statusline
+
+  opt.number = true         -- show current line number
+  opt.relativenumber = true -- show line numbers relative to cursor
+  opt.numberwidth = 4       -- set width of the number column
+  opt.signcolumn = "yes"    -- always display signcolumn
+end
+
+
+do -- hightlight on yank
+  au("TextYankPost", {
+    callback = function()
+      vim.highlight.on_yank {
+        timeout = 200,      -- 200 milliseconds
+        on_macro = true,    -- highlight when executing macro
+      }
+    end
+  })
+end
+
+
+do -- highlight current cursor location
+  opt.cursorline = true
+  local toggle_cursorline = function(event, value)
+    au(event, {
+      group = augroups.user_cursorline_toggler,
+      callback = function() vim.opt_local.cursorline = value end
+    })
+  end
+  toggle_cursorline("WinLeave", false)  -- turn it off when we leave the buffer
+  toggle_cursorline("WinEnter", true)   -- turn it back on when we enter the buffer
+end
+
+
+do -- indenting
+  local width = 2
+  opt.tabstop = width
+  opt.shiftwidth = width
+  opt.softtabstop = width
+  opt.autoindent = true
+  opt.smartindent = true
+end
+
+
+do -- formatting
+  opt.formatoptions = { -- defaults:
+
+    "t",                -- auto-wrap text using "textwidth"
+    "c",                -- auto-wrap comments to "textwidth"; automatically inserts comment leader
+    "q",                -- allow formatting of comment with "gq"
+    "j",                -- remove comment leaders when joining lines
+  }
+end
+
+
+do -- messages
+  opt.shortmess = {     -- defaults:
+
+    "f",                -- "(file 3 of 5)" -> "(3 of 5)"
+    "i",                -- "[Incomplete last line]" -> "[noeol]"
+    "l",                -- "999 lines, 888 bytes" -> "999L, 888B"
+    "n",                -- "[New File]" -> "[New]"
+    "x",                -- "[unix format]" -> "[unix]"
+
+    "t",                -- truncate file message if it is too long to fit on the command line
+    "T",                -- truncate other messages in the middle if they are too long to fit on the command line
+    "o",                -- overwrite message for writing a file with subsequent message for reading a file
+    "O",                -- message for reading a file overwrites any previous message
+    "F",                -- don't give the file info when editing a file
+  }
+  opt.shortmess:append {
+    "I",                -- disable intro message
+    "s",                -- do not show messages when wrapping search
+  }
+end
+
+
+do -- show whitespace
+  opt.list = true
+  opt.listchars:append {
+    space = "⋅",
+    tab = " ",
+    eol = "↲",
+  }
+end
+
+
+do -- scrolling
+  opt.scrolloff = 4
+  opt.mousescroll = {
+    ver = 1,            -- default: 3
+    hor = 1,            -- default: 6
+  }
+end
+
+
+do -- miscellaneous
+  opt.wrap = false
+  opt.breakindent = true
+  opt.linebreak = true
+
+  opt.ignorecase = true
+  opt.smartcase = true
+  opt.clipboard = "unnamedplus" -- integrate with system clipboard
+end
