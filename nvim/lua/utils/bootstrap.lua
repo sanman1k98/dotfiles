7fdc96abc9cf02aeb6ec1f29c72a6f50fe3dff53
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-08-28 10:29:19 -0400

    Refactor bootstrapping functions
    Export `paq_add`, which not only clones Paq, but also adds it to the
    packpath.
    
    `install_plugins` requires the plugin list from the `plugins` module.

--- nvim/lua/utils/bootstrap.lua
+++ nvim/lua/utils/bootstrap.lua
@@ -1,22 +1,31 @@
+local M = {}
 
-local function clone_paq()
+local paq_path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
+local paq_exists = not vim.fn.empty(vim.fn.glob(paq_path)) > 0
+
+M.add_paq = function()
+  if paq_exists then
+    vim.notify("The Paq install directory is not empty: " .. paq_path, vim.log.levels.ERROR)
+    return
+  end
   vim.fn.system {
     "git",
     "clone",
     "--depth=1",
     "https://github.com/savq/paq-nvim.git",
-    vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
+    paq_path
   }
+  vim.cmd.packadd "paq-nvim"
 end
 
-local function bootstrap(plugins)
-  clone_paq()
-  vim.cmd.packadd "paq-nvim"
+M.install_plugins = function()
+  M.add_paq()
   vim.api.nvim_create_autocmd("User", {
     pattern = "PaqDoneInstall",
     command = "quit"
   })
+  local plugins = require "plugins"
   require("paq")(plugins):install()
 end
 
-return bootstrap
+return M
