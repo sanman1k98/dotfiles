d85710653414967038ee754fedd8511ce7b51ef0
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-09-02 14:30:53 -0400

    WIP: define a bunch of user events
    The idea is that this file should define the events and patterns for
    when certain callbacks get triggered, also define when to lazy-load
    certain plugins. Thinking about changing this to "events.lua".

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,7 +1,56 @@
 local au = require "utils.au"
-
-local load_plugins = vim.opt.loadplugins:get()
-
+local doau = require "utils.doau"
 
 
-local patterns
+
+do -- user config
+  local files = {
+    "/lua/keymaps.lua",
+    "/lua/options.lua"
+  }
+  local pattern = vim.tbl_map(function(f)
+    return vim.fn.stdpath("config") .. f
+  end, files)
+
+  au("BufWritePost", {
+    group = au.group.user_config_reload.id,
+    pattern = pattern,
+    callback = doau.user.config_changed
+  })
+end
+
+do -- highlight on yank
+  au("TextYankPost", {
+    group = au.group.user_highlight_on_yank.id,
+    callback = doau.user.on_yank
+  })
+end
+
+do -- toggle cursorline
+  au("WinEnter", {
+    group = au.group.user_toggle_cursorline.id,
+    callback = doau.user.enter_window
+  })
+  au("WinLeave", {
+    group = au.group.user_toggle_cursorline.id,
+    callback = doau.user.leave_window
+  })
+end
+
+
+
+if not vim.opt.loadplugins:get() then return end
+
+
+
+do -- re-register plugins
+  local filename = vim.fn.stdpath("config") .. "/lua/plugins.lua"
+  au("BufWritePost", {
+    group = au.group.user_register_plugins.id,
+    pattern = filename,
+    callback = doau.user.plugins_changed
+  })
+end
+
+
+
