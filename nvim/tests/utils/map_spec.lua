81d040b122ff44209e1ce2baac3826d1c90231e1
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-09-11 13:15:28 +0530

    Add some more tests to `map_spec.lua`
    Add tests that simply demonstrate the usage of the builtin API, vim, and
    Lua functions to add, remove, and list keymappings.

--- nvim/tests/utils/map_spec.lua
+++ nvim/tests/utils/map_spec.lua
@@ -1,11 +1,35 @@
+local mock = require "luassert.mock"
+local stub = require "luassert.stub"
+
+local print = vim.pretty_print
+
 
 describe("Builtin keymap APIs", function()
-  local keymap = vim.keymap
+  local test_leader = " "
+  local test_mode = "n"
+  local test_lhs = "<leader>hi"
+  local test_rhs = function() vim.notify "Hello!" end
 
-  it("Sets a keymapping", function()
-    keymap.set("n", "hi", function()
-      print "Hello!"
-    end)
+  it("Sets the global map leader key as <space>", function()
+    vim.g.mapleader = test_leader
+  end)
+
+  it("Gets all the normal mode keymappings", function()
+    local mapargs = vim.api.nvim_get_keymap(test_mode)
+    print(mapargs)
+  end)
+
+  it("Sets a keymapping that displays \"Hello!\"", function()
+    vim.keymap.set(test_mode, test_lhs, test_rhs)
+  end)
+
+  it("Gets a normal mode keymapping", function()
+    local keymap = vim.fn.maparg(test_lhs, test_mode, false, true)
+    print(keymap)
+  end)
+
+  it("Deletes a keymapping from normal mode", function()
+    vim.keymap.del(test_mode, test_lhs)
   end)
 
 end)
