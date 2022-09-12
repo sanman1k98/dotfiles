51ee8aa049309a86546bb2d38a73625b7f7762a6
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-09-12 09:49:20 +0530

    Add some assertions and remove print statements
    Make some slight changes to the descriptions for the `describe` and `it`
    blocks.
    
    Move the test that gets all the normal-mode keymappings to be the first
    one that gets executed.

--- nvim/tests/utils/map_spec.lua
+++ nvim/tests/utils/map_spec.lua
@@ -4,31 +4,32 @@ local stub = require "luassert.stub"
 local print = vim.pretty_print
 
 
-describe("Builtin keymap APIs", function()
+describe("Demonstrate builtin keymap APIs", function()
   local test_leader = " "
   local test_mode = "n"
   local test_lhs = "<leader>hi"
   local test_rhs = function() vim.notify "Hello!" end
 
-  it("Sets the global map leader key as <space>", function()
+  it("gets all the normal mode keymappings", function()
+    local mapargs = vim.api.nvim_get_keymap(test_mode)
+    assert.is_true(vim.tbl_islist(mapargs))
+  end)
+
+  it("sets the global map leader key as <space>", function()
     vim.g.mapleader = test_leader
   end)
 
-  it("Gets all the normal mode keymappings", function()
-    local mapargs = vim.api.nvim_get_keymap(test_mode)
-    print(mapargs)
-  end)
-
-  it("Sets a keymapping that displays \"Hello!\"", function()
+  it("sets a keymapping that displays \"Hello!\"", function()
     vim.keymap.set(test_mode, test_lhs, test_rhs)
   end)
 
-  it("Gets a normal mode keymapping", function()
-    local keymap = vim.fn.maparg(test_lhs, test_mode, false, true)
-    print(keymap)
+  it("gets a normal mode keymapping", function()
+    local is_abbrv, return_dict = false, true
+    local keymap = vim.fn.maparg(test_lhs, test_mode, is_abbrv, return_dict)
+    assert.is_not_true(vim.tbl_isempty(keymap))
   end)
 
-  it("Deletes a keymapping from normal mode", function()
+  it("deletes a keymapping from normal mode", function()
     vim.keymap.del(test_mode, test_lhs)
   end)
 
