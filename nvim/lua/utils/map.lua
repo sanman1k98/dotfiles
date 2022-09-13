a298dbf6998b6eed4774d62628855d39c58176cd
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-09-13 16:44:26 +0530

    Implement iterator
    Make the tests pass

--- nvim/lua/utils/map.lua
+++ nvim/lua/utils/map.lua
@@ -1,6 +1,17 @@
 local M = {}
 
 function M.args(tbl)
+  local iter = function(t)
+    for mode, mappings in pairs(tbl) do
+      for lhs, def in pairs(mappings) do
+        local desc, rhs, opts = def[1], def[2], def[3]
+        opts = opts or {}
+        opts.desc = desc
+        coroutine.yield(mode, lhs, rhs, opts)
+      end
+    end
+  end
+  return coroutine.wrap(function() iter(tbl) end)
 end
 
 function M.validate(tbl)
