571da129a4ee8795e01448e013ec9baa8538ad7a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-12-20 13:31:54 -0500

    chore(nvim): move main configuration files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,10 @@
--- false if nvim was started with "--noplugin"
-local cmd = vim.cmd
-local au = require "utils.auto"
+require "util.globals"
 
+-- local projects
+vim.opt.rtp:prepend "~/Projects/luauto.nvim"
 
+require "core.options"
+require "core.autocmds"
+require "core.keymaps"
 
-do -- main configuration
-  -- require "events"
-  require "options"
-  require "keymaps"
-
-  cmd.colorscheme "cat_mocha"
-end
-
-
-do -- automatically apply changes without restarting nvim
-  au.user.config_changed(function(t)
-    local file = t.file
-    vim.pretty_print("User config file changed! ", file)
-  end)
-end
-
-
-if vim.opt.loadplugins:get() then           -- false if "nvim --noplugin"
-  local paq_loaded = pcall(require, "paq")
-  if not paq_loaded then
-    local bootstrap = require "utils.bootstrap"
-    bootstrap.add_paq()   -- adds to packpath
-  end
-end
+vim.cmd.colorscheme "cat_mocha"
---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,108 @@
+local auto = require "luauto"
+local autocmd, augroup = auto.cmd, auto.group
+local env = vim.env
+local opt = vim.opt
+
+
+
+-- UI
+if env.TERM == "xterm-kitty" or env.COLORTERM == "truecolor" then
+  opt.termguicolors = true
+end
+
+if vim.o.loadplugins then
+  opt.cmdheight = 0
+end
+
+opt.laststatus = 3        -- global statusline
+
+opt.helpheight = 12
+opt.splitright = true
+opt.splitbelow = true
+
+opt.pumheight = 30        -- max number of items to show in the popup menu
+
+opt.title = true          -- set window title to "titlestring"
+opt.showtabline = 1       -- show tabline for more than one tab
+opt.laststatus = 3        -- global statusline
+
+opt.number = true         -- show current line number
+opt.relativenumber = true -- show line numbers relative to cursor
+opt.numberwidth = 4       -- set width of the number column
+opt.signcolumn = "yes"    -- always display signcolumn
+
+opt.cursorline = true     -- cursorline
+
+
+-- indenting
+local width = 4
+opt.tabstop = width
+opt.shiftwidth = width
+opt.softtabstop = width
+opt.expandtab = false
+opt.autoindent = true
+opt.smartindent = true
+
+
+
+-- formatting
+opt.formatoptions = opt.formatoptions
+
+  + "t"               -- auto-wrap text using "textwidth"
+  + "c"               -- auto-wrap comments to "textwidth"; automatically inserts comment leader
+  + "q"               -- allow formatting of comment with "gq"
+  + "j"               -- remove comment leaders when joining lines
+
+
+
+
+-- messages
+opt.shortmess = opt.shortmess
+
+  + "f"               -- "(file 3 of 5)" -> "(3 of 5)"
+  + "i"               -- "[Incomplete last line]" -> "[noeol]"
+  + "l"               -- "999 lines, 888 bytes" -> "999L, 888B"
+  + "n"               -- "[New File]" -> "[New]"
+  + "x"               -- "[unix format]" -> "[unix]"
+
+  + "t"               -- truncate file message if it is too long to fit on the command line
+  + "T"               -- truncate other messages in the middle if they are too long to fit on the command line
+  + "o"               -- overwrite message for writing a file with subsequent message for reading a file
+  + "O"               -- message for reading a file overwrites any previous message
+  + "F"               -- don't give the file info when editing a file
+
+opt.shortmess = opt.shortmess
+  + "I"               -- disable intro message
+  + "s"               -- do not show messages when wrapping search
+
+
+
+
+-- show whitespace
+opt.list = true
+opt.listchars:append {
+  space = "⋅",
+  tab = " ",
+  eol = "↲",
+}
+
+
+
+-- scrolling
+opt.scrolloff = 4
+opt.mousescroll = {
+  "ver:1",            -- default: 3
+  "hor:1",            -- default: 6
+}
+
+
+
+-- miscellaneous
+opt.wrap = false
+opt.breakindent = true
+opt.linebreak = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.clipboard = "unnamedplus" -- integrate with system clipboard
+
