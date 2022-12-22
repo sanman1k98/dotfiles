0d9989acfa9227ce26a61275ab8e05aea76dcc8a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-12-22 13:17:17 -0500

    refactor(nvim): use `util.map` to set keymaps
    `vim.g.mapleader` must be set before we setup lazy, so move it over to
    `conf.options`.

--- nvim/lua/conf/keymaps.lua
+++ nvim/lua/conf/keymaps.lua
@@ -1,63 +1,13 @@
-local map = require "utils.map"
+local map = require "util.map"
 
--- set <space> as the leader key
-vim.g.mapleader = [[ ]]
+map.insert["kj"]("<esc>")
 
-map {
-	i = {
-		["kj"] = { desc = "quickly leave insert mode",
-			"<esc>",
-		}
-	},
-
-	n = {
-		["<leader>;"] = { desc = "easily enter a command",
-			":",
-		},
-
-		["<leader>h"] = { desc = "search help docs",
-      function() require("telescope.builtin").help_tags() end,
-		},
-
-		["<leader>w"] = { desc = "write file",
-			"<cmd> w <cr>",
-		},
-
-    ["<leader>e"] = { desc = "edit a file",
-      ":e ",
-    },
-
-		["<leader>ff"] = { desc = "fuzzy find files",
-			function() require"telescope.builtin".find_files() end,
-		},
-
-		["<leader>c"] = { desc = "search for a command",
-      function() require("telescope.builtin").commands() end,
-		},
-
-		["<leader>ft"] = { desc = "focus file tree",
-			function()
-				require("nvim-tree.api").tree.focus()
-			end,
-		},
-
-		["<leader>ng"] = { desc = "open neogit",
-			function()
-        require("neogit").open()
-			end,
-		},
-    ["<leader>lc"] = { desc = "toggle displaying whitespace chars",
-      function()
-        vim.opt.list = not vim.opt.list:get()
-      end,
-    },
-	},
-
-  v = {
-    ["<leader>so"] = { desc = "':source' visual selection",
-      function ()
-        vim.notify("TODO: implement this function")
-      end,
-    }
-  }
+map.normal {
+  ["<leader>;"] = ":",
+  ["<leader>w"] = "<cmd>w<cr>",
+  ["<leader>e"] = ":e ",
+  ["<leader>lc"] = function()
+    vim.opt_local.list = not vim.opt_local.list:get()
+  end,
+  ["<leader>lz"] = vim.cmd.Lazy,
 }
