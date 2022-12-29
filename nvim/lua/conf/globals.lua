4fcf8e9c6a3aebac882dbbae1681287a81ac0cf1
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-12-29 17:48:32 -0500

    refactor(nvim)!: `conf.globals`
    The "globals" module now declares and calls a function that sets the
    global Lua values.

--- nvim/lua/conf/globals.lua
+++ nvim/lua/conf/globals.lua
@@ -1,10 +1,12 @@
 local M = {}
 local util = require "util"
 
+_G.CONFIG_DEBUG = nil
+
 M.pp = vim.pretty_print
 
 function M.d(...)
-  if _G.DEBUG == false then return end
+  if _G.CONFIG_DEBUG == false then return end
   local here = debug.getinfo(1, "S")
   local level = 2
   local info = debug.getinfo(level, "S")
@@ -17,13 +19,16 @@ function M.d(...)
   source = vim.loop.fs_realpath(source) or source
   source = vim.fn.fnamemodify(source, ":~:.")..":"..info.linedefined
   local stuff = vim.deepcopy({...})
-  for _, o in pairs(stuff) do
-    util.info(vim.inspect(o))
-  end
+  util.info(vim.inspect { stuff, source = source })
+  -- for _, thing in ipairs(stuff) do
+  --   util.info(vim.inspect(thing))
+  -- end
 end
 
-return function()
+local function set_globals()
   for k, v in pairs(M) do
     _G[k] = v
   end
 end
+
+set_globals()
