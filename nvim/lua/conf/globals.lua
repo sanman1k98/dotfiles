493cb3bd18729d9a3fe9c81e79b73b7193391b0a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-12-30 08:45:40 -0500

    feat(nvim): additional opts to `_G.d`
    This was pretty much copied from folke's dotfiles.

--- nvim/lua/conf/globals.lua
+++ nvim/lua/conf/globals.lua
@@ -16,7 +16,18 @@ local function dump(...)
   local msg = vim.inspect(vim.deepcopy(stuff))
   local loc = get_location()
   return (function()
-    util.notify.info(msg, loc)
+    util.notify(msg, vim.log.levels.INFO, {
+      title = ("$MYVIMRC : %s"):format(loc),
+      on_open = function(win)
+        vim.wo[win].conceallevel = 3
+        vim.wo[win].concealcursor = ""
+        vim.wo[win].spell = false
+        local buf = vim.api.nvim_win_get_buf(win)
+        if not pcall(vim.treesitter.start, buf, "lua") then
+          vim.bo[buf].filetype = "lua"
+        end
+      end,
+    })
   end)
 end
 
