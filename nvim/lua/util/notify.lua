a862f43d10ad7adbbc00f64589c18a92c9d825fd
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-12-30 08:31:54 -0500

    refactor(nvim): `util.notify`
    Instead of directly changing `_G.vim.notify` when setting up the module,
    we will create a notification "backlog" and initialize a check handle
    whose callback will poll to see if the "notify" module provided by
    "nvim-notify" has been loaded or not.
    
    This also prevents Noice from warning us after it loads that "another
    other plugin has changed `vim.notify`", or something to that effect.
    
    Again, this module is heavily inspired by folke's dotfiles.

--- nvim/lua/util/notify.lua
+++ nvim/lua/util/notify.lua
@@ -1,56 +1,53 @@
 local M = {}
 
-local notifier = {
-  backlog = {},
-  builtin = vim.notify,
-}
+local notifier = {}
 
-function notifier.defer(...)
+notifier.backlog = {}
+
+notifier.backend = function(...)
   table.insert(notifier.backlog, {...})
 end
 
-function notifier.init()
-  vim.notify = notifier.defer                           -- set notifier to defer
+notifier.call = function(msg, lvl, opts)
+  opts = opts or {}
+  opts.title = opts.title or "$MYVIMRC"
+  notifier.backend(msg, lvl, opts)
+end
+
+function notifier:setup()
   local checker = vim.loop.new_check()                  -- creates a new check handle
   local start = vim.loop.hrtime()
   checker:start(function()                              -- runs the given callback once per loop iteration
-    local deferring = (vim.notify == notifier.defer)
-    if deferring then
-      if (vim.loop.hrtime() - start) / 1e6 > 1000 then
-        vim.notify = notifier.builtin                   -- use the builtin notifier if it's been a sec
-      else
-        return                                          -- return, we'll call this next loop iteration
-      end
+    if package.loaded.notify then
+      self.backend = package.loaded.notify
+    elseif (vim.loop.hrtime() - start) / 1e6 > 1000 then
+      self.backend = vim.notify
+    else
+      return
     end
     checker:stop()                                      -- stop calling this callback
     vim.schedule(function()                             -- on the main loop,
-      for _, item in ipairs(notifier.backlog) do        -- go through the backlog,
-        vim.notify(unpack(item))                        -- notify user
+      for _, item in ipairs(self.backlog) do            -- go through the backlog,
+        self.call(unpack(item))                         -- notify user
       end
     end)
   end)
 end
 
 function M.info(msg, name)
-  vim.notify(msg, vim.log.levels.INFO, {
-    title = "$MYVIMRC: "..(name or ""),
-  })
+  notifier.call(msg, vim.log.levels.INFO, { title = name })
 end
 
 function M.warn(msg, name)
-  vim.notify(msg, vim.log.levels.WARN, {
-    title = "$MYVIMRC: "..(name or ""),
-  })
+  notifier.call(msg, vim.log.levels.WARN, { title = name })
 end
 
 function M.err(msg, name)
-  vim.notify(msg, vim.log.levels.ERROR, {
-    title = "$MYVIMRC: "..(name or ""),
-  })
+  notifier.call(msg, vim.log.levels.ERROR, { title = name })
 end
 
-notifier.init()
+notifier:setup()
 
 return setmetatable(M, {
-  __call = function(_, ...) vim.notify(...) end,
+  __call = function(_, ...) notifier.call(...) end,
 })
