0a8f9d40ff3df76502e7f28b123b7949e22c38d6
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-03 10:52:30 -0500

    refactor(nvim)!: major keymapping changes
    Don't split up mappings into submodules and move them all together in
    one place.
    
    Also added some more keymaps while trying to cook up a new `util.map`
    function...

--- nvim/lua/conf/keymaps/init.lua
+++ nvim/lua/conf/keymaps/init.lua
@@ -1,29 +1,71 @@
 local util = require "util"
 local map = util.map
+vim.o.timeoutlen = 300
 
+local tele = util.load.mod "telescope.builtin"
+local loaded_wk, wk = pcall(require, "which-key")
+
+if loaded_wk then
+  wk.register {
+    ["<leader>t"] = { name = "+toggles" },
+    ["<leader>f"] = { name = "+fuzzy" },
+    ["<leader>."] = { name = "+config" },
+  }
+end
+
+-- basic insert-mode mappings
 map.insert {
-  ["kj"] = { "<esc>", "leave insert mode" },
+  ["kj"] = "<esc>",
+  ["<c-f>"] = "<right>",
+  ["<c-b>"] = "<left>",
 }
 
-map.normal {
-  ["<leader>;"] = { ":", "enter command-line mode" },
-  ["<leader>w"] = { "<cmd>w<cr>", "Write file" },
-  ["<leader>e"] = { ":e ", "Edit file" },
-  ["<leader>lc"] = { function()
-    vim.opt_local.list = not vim.opt_local.list:get()
-  end, "toggle List Chars" },
-  ["<leader>lz"] = { vim.cmd.Lazy, "show Lazy window" },
-}
+-- some shortcuts
+map.normal({
+  [";"] = { ":", "enter command-line" },
+  ["w"] = { vim.cmd.write, "write file" },
+  ["L"] = { vim.cmd.Lazy, "lazy.nvim"},
+}, { prefix = "<leader>"})
+
+-- config
+map.normal({
+  ["P"] = { desc = "Profile startup with Lazy",
+    function() require("lazy.view.commands").commands["profile"]() end,
+  }
+}, { prefix = "<leader>."})
+
+-- toggles
+map.normal({
+  ["lc"] = { desc = "list chars",
+    function() vim.opt_local.list = not vim.opt_local.list:get() end,
+  },
+  ["e"] = { desc = "file explorer",
+    "<cmd>Neotree toggle filesystem<cr>"
+  },
+  ["nc"] = { desc = "number column",
+    function()
+      local win_opt = vim.wo[0]
+      local set = win_opt.number
+      win_opt.number = not set
+      win_opt.relativenumber = not set
+    end,
+  },
+}, { prefix = "<leader>t"})
+
+-- fuzzy
+map.normal({
+  ["<cr>"] = { vim.cmd.Telescope, "Telescope builtin" },
+  ["f"] = { function() tele.find_files() end, "find files" },
+  ["h"] = { function() tele.help_tags() end, "search help tags" },
+  ["b"] = { function() tele.buffers() end, "open buffers" },
+  [";"] = { function() tele.commands() end, "commands" },
+  ["r"] = { function() tele.reloader() end, "reload Lua modules" },
+  ["o"] = { function() tele.vim_options() end, "search Vim options" },
+  ["k"] = { function() tele.keymaps() end, "keymaps" },
+}, { prefix = "<leader>f" })
 
 map.visual {
   ["<"] = { "<gv" },
   [">"] = { ">gv" },
 }
 
-local keymaps = util.mod.submods()  -- returns a function
-
-keymaps "neo-tree"
-keymaps "telescope"
-keymaps "which-key"
-
-return keymaps
--- nvim/lua/util/map.lua
+++ nvim/lua/util/map.lua
@@ -1,24 +1,136 @@
-local map = {}
-local Mode = {}
+local notify = require "util.notify"
+local M = {}
+local Mode, ModeIndex = {}, {}
+
+local MYALIASES = {
+  normal      = "n",
+  visual      = "v",  -- actually visual and select mode
+  select      = "x",
+  op_pending  = "o",
+  insert      = "i",
+  command     = "c",
+  terminal    = "t",
+}
+
+-- false if not actual mapargs
+local OPTKEYS = {
+  -- [1]               = false,
+  -- [2]               = false,
+  prefix            = false,
+  name              = false,
+  cond              = false,
+  mode              = false,
+  buffer            = true,
+  desc              = true,
+  expr              = true,
+  nowait            = true,
+  unique            = true,
+  -- noremap           = true,
+  -- replace_keycodes  = true,
+}
+
+local function optkey(k)
+  return OPTKEYS[k] ~= nil
+end
 
 local function merge(...)
   return vim.tbl_extend("force", ...)
 end
 
+-- local function maparg(k)
+--   return OPTKEYS[k]
+-- end
+
+--- Get the opts from a group of mappings
+---@param mappings table
+local function getopts(mappings)
+  local opts = {}
+  for k, v in pairs(mappings) do
+    if optkey(k) then opts[k] = v end
+  end
+  if vim.is_callable(opts.cond) then
+    opts.cond = opts.cond()
+  end
+  return opts
+end
+
+--- Traverse a group of mappings structured as a prefix-tree and generate lhs,
+--- rhs, and opts for each valid mapping.
+---@param mappings table: prefix-tree node
+---@param opts table|nil: overrides opts found in mappings
+local function parse(mappings, opts)
+  opts = merge(getopts(mappings), opts or {})
+  assert(opts.mode)
+  if opts.cond ~= nil and not opts.cond then
+    assert(not vim.is_callable(opts.cond))
+    return
+  end
+  if not opts.prefix then -- we're at the root of the group
+    opts.prefix = opts.prefix or ""
+  end
+  local co = coroutine
+  return co.wrap(function()
+    local subgroups = {}  -- subnodes/children to parse
+    for k, info in pairs(mappings) do -- iter through this node
+      local lhs = opts.prefix..k
+      if k == "name" then -- wk group name
+        co.yield(lhs, nil, merge(opts, { name = info }))
+      elseif type(info) == "string" or vim.is_callable(info) then -- rhs value for mapping
+        co.yield(lhs, info, opts)
+      elseif info == false then -- del keymap
+        co.yield(lhs, false, opts)
+      elseif not optkey(k) and type(info) == "table" then -- 
+        if info[1] then -- has rhs value for mapping
+          co.yield(lhs, info[0], merge(opts, info))
+        else  -- subnode to parse
+          subgroups[lhs] = info
+        end
+      end
+    end
+    for prefix, subgroup in pairs(subgroups) do -- go through children
+      opts.prefix = prefix
+      for lhs, rhs, o in parse(subgroup, opts) do -- recurse (maybe not technically?)
+        co.yield(lhs, rhs, o)
+      end
+    end
+  end)
+end
+
+M.mode = {}
+
+for alias, shortname in pairs(MYALIASES) do
+  M.mode[alias] = function(mappings, opts)
+    opts = opts or {}
+    opts.mode = shortname
+    local parsed = {}
+    for lhs, rhs, args in parse(mappings, opts) do
+      parsed[lhs] = { rhs, args }
+    end
+    d(parsed)
+  end
+end
+
 local function use_vim(mappings, opts)
-  local set = vim.keymap.set
+  local ret = {}
   local mode, prefix = opts.mode, opts.prefix or ""
   opts.mode, opts.prefix = nil, nil
   for lhs, info in pairs(mappings) do
     lhs = prefix..lhs
-    if type(info) == "table" then
+    local is_rhs = type(info) == "string" or vim.is_callable(info)
+    if is_rhs then
+      vim.keymap.set(mode, lhs, info, opts)
+    elseif type(info) == "table" then
       local rhs, desc = info[1], info[2] or info.desc
       info[1], info[2] = nil, nil
-      set(mode, lhs, rhs, merge(opts, info, { desc = desc }))
-    else
-      set(mode, lhs, info, opts)
+      vim.keymap.set(mode, lhs, rhs, vim.tbl_extend("force", opts, info, { desc = desc }))
+    elseif info == false then
+      vim.keymap.del(mode, lhs, opts)
+    end
+    if info ~= false then
+      table.insert(ret, { lhs, mode = mode })
     end
   end
+  return ret
 end
 
 function Mode:__call(mappings, opts)
@@ -27,21 +139,8 @@ function Mode:__call(mappings, opts)
   use_vim(mappings, opts)
 end
 
-local aliases = {
-  normal      = "n",
-  visual      = "v",  -- actually visual and select mode
-  op_pending  = "o",
-  insert      = "i",
-  command     = "c",
-  terminal    = "t",
-}
+for alias, shortname in pairs(MYALIASES) do
+  M[alias] = setmetatable({ _mode = shortname }, Mode)
+end
 
-return setmetatable(map, {
-  __index = function(_, k)
-    if #k > 1 then
-      k = aliases[k]
-    end
-    local shortname = k
-    return setmetatable({ _mode = shortname }, Mode)
-  end,
-})
+return M
