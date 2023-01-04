1bf4bf0f740c216e8da87bde97d2273e5c3dea5f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-04 11:25:37 -0500

    refactor(nvim): use `util.load`

--- nvim/lua/conf/init.lua
+++ nvim/lua/conf/init.lua
@@ -1,7 +1,1 @@
-local submods = require("util.mod").submods()
-
-return submods
-
--- Note: calling `require` in return statement will not work! This is because
--- when `util.mod.submods()` calls `debug.getinfo()` to get the path to this
--- file, the "source" field will be "[C]".
+return require("util.load").submods("conf")
