db31de10e10f4a5d7c7fb4805fa981900829f74a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-04 11:30:55 -0500

    feat(nvim): LSP `on_attach`
    Refactor setup handlers to use the same `on_attach` callback which sets
    keymaps.
    
    Also add more keymaps that leverage LSP functionality.

--- nvim/lua/conf/keymaps/lsp.lua
+++ nvim/lua/conf/keymaps/lsp.lua
@@ -1,10 +1,28 @@
-return function(_, bufnr)
-  require("util.map").normal({
-    ["<leader>vr"] = { vim.lsp.buf.rename, "rename symbol" },
+local notify = require "util.notify"
+local map = require "util.map"
+
+---@param client table
+return function(client, bufnr)
+  local capabilities = client.server_capabilities
+  local has_hover = capabilities.hoverProvider and true or false
+
+  map.normal({
+    ["<leader>rn"] = { vim.lsp.buf.rename, "rename symbol" },
     ["<leader>wl"] = { desc = "list workspace folders",
       function()
-        vim.notify(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        notify.info(vim.inspect(vim.lsp.buf.list_workspace_folders()), "LSP: workspace folders")
       end,
-    }
+    },
+    ["gD"] = { vim.lsp.buf.declaration, "goto declaration" },
+    ["gd"] = { vim.lsp.buf.definition, "goto definition" },
+    ["gr"] = { vim.lsp.buf.references, "list all references" },
+    ["gi"] = { vim.lsp.buf.implementation, "goto implementation" },
+    ["<c-k>"] = { vim.lsp.buf.signature_help, "display signature help" },
+    ["K"] = { desc = "display hover info",
+      function()
+        if has_hover then vim.lsp.buf.hover()
+        else notify.warn("Hover not supported", "LSP")end
+      end,
+    },
   }, { buffer = bufnr })
 end
