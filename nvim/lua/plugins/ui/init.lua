d28b043b2e361102e7c7c6e3861eb82749d0d864
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-04 15:22:02 -0500

    feat(nvim): add a dashboard
    Adapted from the one found in folke/LazyVim.
    
    Refactor UI plugin specs and disable feline's winbar.

--- nvim/lua/plugins/ui/init.lua
+++ nvim/lua/plugins/ui/init.lua
@@ -1,71 +1,59 @@
-local noice = {
-  "folke/noice.nvim",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-  },
-  event = "VeryLazy",
-}
+return {
+  -- icons
+  "nvim-tree/nvim-web-devicons",
 
-noice.config = function()
-  require("noice").setup {
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+  -- UI
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    event = "VeryLazy",
+    config = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        lsp_doc_border = false,
       },
     },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      lsp_doc_border = false,
+  },
+
+  -- statusline
+  {
+    "feline-nvim/feline.nvim",
+    event = "VeryLazy",
+    config = require "plugins.ui.feline",
+  },
+
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+  },
+
+  -- dashboard
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = require "plugins.ui.alpha",
+  },
+
+  -- screensaver
+  {
+    "folke/drop.nvim",
+    enabled = false,
+    event = "VimEnter",
+    config = {
+      theme = "stars",
     },
-  }
-end
+  },
 
-local bufferline = {
-  "akinsho/bufferline.nvim",
-  -- event = "VeryLazy",
 }
 
-local statusline = {
-  "feline-nvim/feline.nvim",
-  event = "VimEnter",
-}
-
-statusline.config = function()
-  local comps = nil
-  local cat = package.loaded.catppuccin or pcall(require, "catppuccin")
-  if cat then
-    comps = require("catppuccin.groups.integrations.feline").get()
-  end
-  local feline = require "feline"
-  feline.setup({  -- TODO: customize this
-    components = comps,
-  })
-  feline.winbar.setup()
-end
-
-local icons = {
-  "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("nvim-web-devicons").setup()
-  end,
-}
-
-local dashboard = {
-  "folke/drop.nvim",
-  -- event = "VimEnter",
-  config = function()
-    require("drop").setup()
-  end,
-}
-
-return {
-  icons,
-  noice,
-  statusline,
-  bufferline,
-  dashboard,
-}
