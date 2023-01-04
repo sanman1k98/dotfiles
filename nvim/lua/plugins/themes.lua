9e878f2caac2fea360046d8d25bee441b79bfb35
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-04 11:48:15 -0500

    feat(nvim): make catppuccin a start plugin

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,6 +1,11 @@
 return {  -- configurations are in the colors directory 
-  { "catppuccin/nvim", name = "catppuccin" },
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "catppuccin/nvim", name = "catppuccin",
+    lazy = false, priority = 1000,
+  },
+  {
+    "rose-pine/neovim", name = "rose-pine",
+  },
   "projekt0n/github-nvim-theme",
   "folke/tokyonight.nvim",
   "rebelot/kanagawa.nvim",
