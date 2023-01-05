96cb53241b08b1e899629eda1ec9508d670a6452
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-05 13:49:20 -0500

    feat: cleaner looking file browser

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,22 +1,18 @@
-local neotree = {
+return {
   "nvim-neo-tree/neo-tree.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  cmd = "Neotree"
-}
-
-neotree.config = function()
-  vim.g.neo_tree_remove_legacy_commands = 1
-  require("neo-tree").setup {
+  cmd = "Neotree",
+  init = function()
+    vim.g.neo_tree_remove_legacy_commands = 1
+  end,
+  config = {
+    add_blank_line_at_top = true,
+    hide_root_node = true,
+    retain_hidden_root_indent = true,
+    window = { width = 30 },
     default_component_configs = {
       indent = {
         with_markers = false,
       }
     },
-  }
-end
-
-return neotree
+  },
+}
