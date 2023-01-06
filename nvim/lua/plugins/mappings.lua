d69749a7d6358d8d463e8ef87fde6b0bfba7c44b
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-06 13:04:36 -0500

    feat(nvim): define telescope mappings in spec
    Use `util.map.set()` functions in the plugin spec which return a list of
    `LazyKeys`. This allows lazy to track more precisely how plugins are
    loaded.

--- nvim/lua/plugins/mappings.lua
+++ nvim/lua/plugins/mappings.lua
@@ -16,17 +16,6 @@ local function mappings()
       [";"] = { ":", "enter command-line" },
       w = { vim.cmd.write, "write file" },
       l = { vim.cmd.Lazy, "lazy.nvim"},
-      f = {
-        name = "fuzzy",
-        ["<cr>"] = { vim.cmd.Telescope, "Telescope builtin" },
-        [";"] = { function() require("telescope.builtin").commands() end, "commands" },
-        f = { function() require("telescope.builtin").find_files() end, "find files" },
-        h = { function() require("telescope.builtin").help_tags() end, "search help tags" },
-        b = { function() require("telescope.builtin").buffers() end, "open buffers" },
-        r = { function() require("telescope.builtin").reloader() end, "reload Lua modules" },
-        o = { function() require("telescope.builtin").vim_options() end, "search Vim options" },
-        k = { function() require("telescope.builtin").keymaps() end, "keymaps" },
-      },
       t = {
         name = "toggles",
         ["lc"] = { desc = "list chars",
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,18 @@
 return {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
-  -- TODO define keys property
+  keys = require("util.map").set {  -- returns LazyKeys[]
+    prefix = "<leader>f",
+    name = "fuzzy",
+    ["<cr>"] = { vim.cmd.Telescope, "Telescope builtin" },
+    [";"] = { function() require("telescope.builtin").commands() end, "commands" },
+    f = { function() require("telescope.builtin").find_files() end, "find files" },
+    h = { function() require("telescope.builtin").help_tags() end, "search help tags" },
+    b = { function() require("telescope.builtin").buffers() end, "open buffers" },
+    r = { function() require("telescope.builtin").reloader() end, "reload Lua modules" },
+    o = { function() require("telescope.builtin").vim_options() end, "search Vim options" },
+    k = { function() require("telescope.builtin").keymaps() end, "keymaps" },
+  },
   config = {
     -- TODO
   },
