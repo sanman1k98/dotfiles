0b201162a571df3172548f6b09d252194b9aa0b8
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-06 20:38:36 -0500

    refactor(nvim): `init.lua`

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,26 @@
+local settings = require "user.settings"
+
 require "conf.options"
-require "conf.lazy"
 require "conf.globals"
+
+require("util.lazy").setup("plugins", {
+  root = settings.pkg_root,
+  defaults = { lazy = true },
+  install = {
+    colorscheme = { "tokyonight", "habamax" },
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "zipPlugin",
+        "tutor",
+        "tohtml",
+      }
+    }
+  },
+})
 
 vim.cmd.colorscheme "tokyonight"
