347882ea33e46176226ac069c212eb541d543aff
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-06 20:33:48 -0500

    feat: LSP configuration options

---
+++ nvim/lua/plugins/lsp/servers.lua
@@ -0,0 +1,15 @@
+return {
+  -- lua-language-server
+  sumneko_lua = {
+    settings = {
+      Lua = {
+        workspace = {
+          checkThirdParty = false,
+        },
+        completion = {
+          callSnippet = "Replace",
+        }
+      }
+    }
+  },
+}
