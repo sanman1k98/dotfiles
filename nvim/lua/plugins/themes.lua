efd3e11613042da06f00363197082368c90dc7c4
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-06 20:35:48 -0500

    refactor: specify Tokyonight as start

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,13 +1,15 @@
 return {  -- configurations are in the colors directory 
   {
     "catppuccin/nvim", name = "catppuccin",
-    lazy = false, priority = 1000,
   },
   {
     "rose-pine/neovim", name = "rose-pine",
   },
   "projekt0n/github-nvim-theme",
-  "folke/tokyonight.nvim",
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, priority = 1000,
+  },
   "rebelot/kanagawa.nvim",
   "EdenEast/nightfox.nvim",
 }
