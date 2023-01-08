c746f0330cfd30415eaf8bf1fa45b2822d41829f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-07 19:45:20 -0500

    feat!: treesitter plugins

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,42 +1,51 @@
-local ts = {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
+return {
+  -- a bunch of treesitter queries
+  { -- adapted from LazyVim
     "nvim-treesitter/nvim-treesitter-textobjects",
-    "nvim-treesitter/nvim-treesitter-context",
+    init = function()
+      require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+    end,
   },
-  build = ":TSUpdate",
-  event = "BufReadPost",
-}
 
-ts.config = function()
-  require("nvim-treesitter.configs").setup {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "bash",
-      "regex",
-      "markdown",
-      "markdown_inline",
+  {
+    "chrisgrieser/nvim-various-textobjs",
+    config = {
+      useDefaultKeymaps = false,
     },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    -- can be enabled with ":TSBufEnable incremental_selection"
-    incremental_selection = {
-      enable = true,
-      keymaps = { -- default keymaps
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
-      },
-    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = "BufReadPost",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "lua",
+          "vim",
+          "bash",
+          "regex",
+          "markdown",
+          "markdown_inline",
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+        -- can be enabled with ":TSBufEnable incremental_selection"
+        -- incremental_selection = {
+        --   enable = true,
+        --   keymaps = { -- default keymaps
+        --     init_selection = "gnn",
+        --     node_incremental = "grn",
+        --     scope_incremental = "grc",
+        --     node_decremental = "grm",
+        --   },
+        -- },
+      }
+    end,
   }
-  vim.g.ts_highlight_lua = true
-  -- setup context after
-  require("treesitter-context").setup()
-end
-
-return ts
+}
