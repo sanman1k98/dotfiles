e2598e3c663e3ec493c9a1c80ffc479ce8d5d813
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-10 19:26:35 -0500

    feat: configure git plugins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,41 +1,90 @@
-local gitsigns = {
-  "lewis6991/gitsigns.nvim",
-  event = "BufReadPre"
-}
+local map = require "util.map"
 
-gitsigns.config = function()
-  local gs = require "gitsigns"
-  gs.setup {
-    -- TODO: configuration and keymaps
-  }
-end
-
-local diffview = {
-  "sindrets/diffview.nvim",
-  cmd = {
-    "DiffviewOpen",
-    "DiffviewClose",
-    "DiffviewTogleFiles",
-    "DiffviewFocusFiles",
-  },
-  config = function()
-    require("diffview").setup()
-  end,
-}
-
-local neogit = {
-  "TimUntersberger/neogit",
-  cmd = "Neogit",
-  config = function()
-    local ng = require "neogit"
-    ng.setup {
-      -- TODO
-    }
-  end,
+-- which-key group labels
+map.set {
+  prefix = "<leader>g",
+  name = "git",
+  d = { name = "diff" },
+  h = { name = "hunk" },
 }
 
 return {
-  gitsigns,
-  diffview,
-  neogit,
+  -- diffview
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+    },
+    keys = map.lazykeys({
+      prefix = "<leader>g",
+      d = {
+        h = {
+          desc = "history",
+          vim.cmd.DiffviewFileHistory,
+        },
+        i = {
+          desc = "against staged",
+          function() vim.cmd.DiffviewOpen "--staged" end,
+        },
+        c = {
+          desc = "against HEAD",
+          vim.cmd.DiffviewOpen,
+        },
+      },
+    }),
+    opts = {
+      enhanced_diff_hl = true,
+      show_help_hints = false,
+      file_panel = {
+        listing_style = "list",
+        win_config = {
+          width = 30
+        },
+      },
+    },
+  },
+
+  -- gitsigns
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    opts = function()
+      local gs = require "gitsigns"
+      local keymaps = {
+        prefix = "<leader>g",
+        h = {
+          name = "hunk",
+          p = {
+            desc = "preview inline",
+            function() gs.preview_hunk_inline() end,
+          },
+          s = {
+            desc = "stage",
+            function() gs.stage_hunk() end,
+          },
+          u = {
+            desc = "undo stage",
+            function() gs.undo_stage_hunk() end,
+          },
+          l = {
+            desc = "current buffer",
+            function() gs.setloclist() end,
+          },
+        },
+      }
+      return {
+        base = "HEAD",
+        on_attach = function()
+          map.set(keymaps)
+        end,
+      }
+    end,
+  },
+
+  -- neogit
+  {
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
+    config = true,
+  },
 }
