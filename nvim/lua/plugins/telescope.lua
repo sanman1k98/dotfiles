137c9986aad17e0bfeb6638578d66603968122ab
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-10 19:25:18 -0500

    feat: changes telescope keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,16 +2,20 @@ return {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
   keys = require("util.map").lazykeys {  -- returns LazyKeys[]
-    prefix = "<leader>f",
-    name = "fuzzy",
-    ["<cr>"] = { vim.cmd.Telescope, "Telescope builtin" },
-    [";"] = { function() require("telescope.builtin").commands() end, "commands" },
-    f = { function() require("telescope.builtin").find_files() end, "find files" },
-    h = { function() require("telescope.builtin").help_tags() end, "search help tags" },
-    b = { function() require("telescope.builtin").buffers() end, "open buffers" },
-    r = { function() require("telescope.builtin").reloader() end, "reload Lua modules" },
-    o = { function() require("telescope.builtin").vim_options() end, "search Vim options" },
-    k = { function() require("telescope.builtin").keymaps() end, "keymaps" },
+    ["<leader>f"] = {
+      name = "fuzzy",
+      ["<cr>"] = { vim.cmd.Telescope, "Telescope builtin" },
+      f = { function() require("telescope.builtin").find_files() end, "find files" },
+      b = { function() require("telescope.builtin").buffers() end, "open buffers" },
+      r = { function() require("telescope.builtin").reloader() end, "reload Lua modules" },
+    },
+    ["<leader>h"] = {
+      name = "help",
+      [";"] = { function() require("telescope.builtin").commands() end, "commands" },
+      t = { function() require("telescope.builtin").help_tags() end, "search help tags" },
+      k = { function() require("telescope.builtin").keymaps() end, "keymaps" },
+      o = { function() require("telescope.builtin").vim_options() end, "search Vim options" },
+    },
   },
   config = {
     -- TODO
