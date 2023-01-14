258e232c4080d199c7960b59ee329f877bf61886
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-14 12:01:20 -0500

    fix(nvim): formatting Lua comments

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,8 +1,15 @@
 
 local indent = 2
-local opt = vim.opt
+local opt = vim.opt_local
 
 opt.tabstop = indent
 opt.shiftwidth = indent
 opt.softtabstop = indent
 opt.expandtab = true
+
+-- Allow cmments to start with 2 or 3 dashes for annotating. Makes formatting
+-- descriptions with `gw` easier.
+opt.comments = {
+  ":---",
+  ":--",
+}
