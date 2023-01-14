a36633462379d7235ee7361f646a192e1327b2e2
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-14 17:12:33 -0500

    refactor: `zshrc`

--- zsh/init/completion.zsh
+++ zsh/init/completion.zsh
@@ -1,8 +1,12 @@
+# add brew completions to fpath before compinit is called
+fpath+="${HOMEBREW_PREFIX}/share/zsh/site-functions"
+
 autoload -Uz compinit
 compinit -d "${XDG_CACHE_HOME}/zsh/zcompdump"
 
 zstyle ":completion:*" completer _extensions _complete _approximate
 
+# case insensitive completion
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
 
-source "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+test -d "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" && source "$_"
--- zsh/init/prompt.zsh
+++ zsh/init/prompt.zsh
@@ -1,2 +1,2 @@
-source "${HOMEBREW_PREFIX}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+test -d "${HOMEBREW_PREFIX}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" && source "$_"
 eval "$(starship init zsh)"
