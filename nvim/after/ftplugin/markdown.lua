da1d3c5af2cbd50d9d96bd8ef9dbf49e97c6f6aa
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-16 18:14:43 -0500

    feat: set options for markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,12 @@
-local opt = vim.opt
+local opt = vim.opt_local
 
 opt.number = false
 opt.relativenumber = false
 opt.wrap = true
+opt.expandtab = true
+opt.list = true
+
+local width = 2
+opt.tabstop = width
+opt.softtabstop = width
+opt.shiftwidth = width
