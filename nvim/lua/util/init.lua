df8f7dfbca89678141905ecc4b34f03201484c37
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-20 14:05:00 -0500

    feat: add `util.toggle()`

--- nvim/lua/util/init.lua
+++ nvim/lua/util/init.lua
@@ -40,4 +40,42 @@ function util.setup()
   vim.opt.rtp:prepend(lazypath)
 end
 
+--- Toggle an option or options; notify user unless `silent` is specified. Can
+--- specify an option to toggle as a string or list of strings. Can also
+--- specify an option to toggle between two values by passing a table a key as
+--- the option name, and a list containing two items as its corresponding
+--- value.
+---@param option string|string[]|table<string, {[1]:any, [2]:any}>
+---@param silent? boolean defaults to false
+function util.toggle(option, silent)
+  silent = silent == true
+  local opt = vim.opt_local
+  local msg = not silent and { "# Toggled option:\n" } or nil
+  if type(option) == "string" then option = { option } end
+  for k, v in pairs(option) do
+    local name, old, new
+    if type(k) == "number" then
+      name = v
+      old = opt[name]:get()
+      new = not old
+      opt[name] = new
+    else
+      name = k
+      old = opt[name]:get()
+      if old == v[1] then
+        opt[name] = v[2]
+        new = v[2]
+      else
+        opt[name] = v[1]
+        new = v[1]
+      end
+    end
+    if msg then
+      table.insert(msg, ("- *%s*: `%s` -> `%s`"):format(name, old, new))
+    end
+  end
+  if not msg then return end
+  util.notify.info(table.concat(msg, "\n"))
+end
+
 return util
