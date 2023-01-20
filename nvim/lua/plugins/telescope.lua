26f1069d7b4d6acf7d7e3ff3118cff36aec8152d
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-20 14:09:58 -0500

    feat: telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -17,7 +17,22 @@ return {
       o = { function() require("telescope.builtin").vim_options() end, "search Vim options" },
     },
   },
-  config = {
-    -- TODO
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.55,
+          results_width = 0.8,
+        },
+        vertical = {
+          mirror = false,
+        },
+        width = 0.87,
+        height = 0.80,
+        preview_cutoff = 120,
+      },
+    },
   },
 }
