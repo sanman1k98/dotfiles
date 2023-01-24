4353c4d022627e107bdb5d39ff6fe647e432ffaa
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-24 13:32:03 -0500

    feat: override `dawnfox` highlight groups

--- nvim/colors/dawnfox.lua
+++ nvim/colors/dawnfox.lua
@@ -1,12 +1,18 @@
-require("nightfox.config").set_fox("dawnfox")
 local fox = require "nightfox"
 
 fox.setup({
   options = {
     styles = {
       comments = "italic",
-    }
-  }
+    },
+  },
+  groups = {
+    dawnfox = {
+      WhiteSpace  = { fg = "bg4" },    -- make 'list' characters slightly more visible (default: "bg3")
+      EndOfBuffer = { fg = "fg3" },    -- show filler lines after the end of the buffer (default: "bg1" which is the background)
+    },
+  },
 })
 
+require("nightfox.config").set_fox("dawnfox")
 fox.load()
