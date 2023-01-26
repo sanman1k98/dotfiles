d5ccf5717f0ef8a690e027a6c34fc6f25817b87d
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-26 13:20:41 -0500

    feat: telescope results appear at top

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -20,6 +20,7 @@ return {
   opts = {
     defaults = {
       layout_strategy = "horizontal",
+      sorting_strategy = "ascending",
       layout_config = {
         horizontal = {
           prompt_position = "top",
