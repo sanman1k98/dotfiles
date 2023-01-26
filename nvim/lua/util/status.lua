9c45452ff025b43d85d2c5344611deae1660da31
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-26 12:58:15 -0500

    feat: respect `signcolumn` option

--- nvim/lua/util/status.lua
+++ nvim/lua/util/status.lua
@@ -11,7 +11,9 @@ function M.get_signs()
   end, vim.fn.sign_getplaced(buf, { group = "*", lnum = vim.v.lnum })[1].signs)
 end
 
-function M.column()
+-- TODO: refactor this whole thing
+
+function M.eval()
   local sign, git_sign
   for _, s in ipairs(M.get_signs()) do
     if s.name:find("GitSign") then
@@ -21,25 +23,39 @@ function M.column()
     end
   end
 
-  local nu = " "
-  local number = vim.api.nvim_win_get_option(vim.g.statusline_winid, "number")
-  if number and vim.wo.relativenumber and vim.v.virtnum == 0 then
-    nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+  local number_comp = ""
+  local sign_comp = ""
+  local git_comp = ""
+  local opts = { win = vim.g.statusline_winid }
+  local nu = vim.api.nvim_get_option_value("nu", opts)
+  local rnu = vim.api.nvim_get_option_value("rnu", opts)
+  local signcolumn = vim.api.nvim_get_option_value("scl", opts)
+  if nu and rnu and vim.v.virtnum == 0 then
+    number_comp = (vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum).." "
+  end
+  if signcolumn ~= "no" then
+    sign_comp = sign and ("%#"..sign.texthl.."#"..sign.text.."%*") or " "
+    git_comp = git_sign and ("%#"..git_sign.texthl.."#"..git_sign.text.."%*") or "  "
   end
   local components = {
-    sign and ("%#" .. sign.texthl .. "#" .. sign.text .. "%*") or " ",
-    [[%=]],
-    nu .. " ",
-    git_sign and ("%#" .. git_sign.texthl .. "#" .. git_sign.text .. "%*") or "  ",
+    sign_comp,
+    [[%=]],   -- spacing
+    number_comp,
+    git_comp,
   }
   return table.concat(components, "")
+end
+
+-- TODO: implement some toggles
+
+function M.toggle()
 end
 
 function M.setup()
   if M.did_setup then return end
   if vim.fn.has("nvim-0.9.0") == 1 then
-    _G.Status = M
-    vim.opt.statuscolumn = [[%!v:lua.Status.column()]]
+    _G.Gutter = M
+    vim.go.statuscolumn = [[%!v:lua.Gutter.eval()]]
   end
   M.did_setup = true
 end
