0ace487ad65723c593525db93444f2247dc90466
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-26 13:21:07 -0500

    style: rename functions

--- nvim/lua/plugins/autocmds.lua
+++ nvim/lua/plugins/autocmds.lua
@@ -1,13 +1,13 @@
 local function autocmds()
   require("luauto").setup()
 
-  local function set_cul(set)
+  local function cul(set)
     return (function()
       vim.opt_local.cul = set
     end)
   end
 
-  local function cul_hl(set)
+  local function culopt(set)
     return (function()
       vim.opt_local.culopt = set
     end)
@@ -16,17 +16,17 @@ local function autocmds()
   vim.augroup.cursorline(function(au)
     au:clear()
     -- enable cursorline for the active window
-    au.WinEnter(set_cul(true))
-    au.WinLeave(set_cul(false))
+    au.WinEnter(cul(true))
+    au.WinLeave(cul(false))
     -- only highlight the number in insert mode
-    au.InsertEnter(cul_hl({ "number" }))
-    au.InsertLeave(cul_hl({ "number", "line" }))  -- default
+    au.InsertEnter(culopt({ "number" }))
+    au.InsertLeave(culopt({ "number", "line" }))  -- default
     -- disbale cursorline for these filetypes
     au.FileType[{
       "alpha",
       "mason",
       "TelescopePrompt",
-    }](set_cul(false))
+    }](cul(false))
   end)
 
   -- close certain filetypes with `q`
