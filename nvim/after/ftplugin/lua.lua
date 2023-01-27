6225f66d5c1bc708ac733f7f202a7901e4fc7fad
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-27 17:01:41 -0500

    style: fix typo

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -3,7 +3,7 @@ local opt = vim.opt_local
 opt.tabstop = 2
 opt.expandtab = true
 
--- Allow cmments to start with 2 or 3 dashes for annotating. Makes formatting
+-- Allow comments to start with 2 or 3 dashes for annotating. Makes formatting
 -- descriptions with `gw` easier.
 opt.comments = {
   ":---",
