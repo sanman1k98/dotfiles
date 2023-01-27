39b48dc9687233f6972f6e5f40de0bf5599c0eba
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-27 17:01:26 -0500

    feat: change `listchars`

--- nvim/lua/conf/options.lua
+++ nvim/lua/conf/options.lua
@@ -72,6 +72,8 @@ opt.listchars = {
   trail          = "~",    -- trailing whitespace
   eol            = "↲",    -- end of line
   space          = "⋅",    -- a single <space>
+  leadmultispace = "⋅⋅⋅•", -- used cyclically to show multiple consecutive leading spaces; overrides "space"
+  tab            = "  ",  -- 3 chars `xyz` to show a <tab>; `z` is always used, `x` is prepended, `y` is used as many times as can fit
 }
 
 -- characters to use when displaying special items
