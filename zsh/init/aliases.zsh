70652b425816a97fbc9fa00e7d8c4f902853d26f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-29 20:08:07 -0500

    feat(zsh): alias to change theme

--- zsh/init/aliases.zsh
+++ zsh/init/aliases.zsh
@@ -1,5 +1,8 @@
-
 alias la="ls -lAG --color=always"
 alias nv="nvim"
 alias vi="nvim --noplugin"
 alias lg="lazygit"
+
+if [[ "${TERM}" == "xterm-kitty" ]]; then
+	alias theme="kitty +kitten themes --reload-in=all $*"
+fi
