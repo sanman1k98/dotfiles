29f45df84d9ad00911029efea1360dc3838b7d16
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-10-07 12:36:54 -0400

    feat(zsh): add an alias for `lazyvim`

--- zsh/init/aliases.zsh
+++ zsh/init/aliases.zsh
@@ -1,6 +1,7 @@
 alias la="ls -lAG --color=always"
 alias nv="nvim"
 alias vi="nvim --noplugin"
+alias lv="NVIM_APPNAME=lazyvim nvim"
 alias lg="lazygit"
 
 # if [[ "${TERM}" == "xterm-kitty" ]]; then
