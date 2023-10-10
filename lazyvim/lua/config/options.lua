7abbfbb08111590585884269aeb208be6fb655df
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-10-10 11:17:45 -0400

    feat: specify `listchars` option

---
+++ lazyvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+-- characters to display whitespace
+opt.listchars = {
+  trail          = "~",    -- trailing whitespace
+  eol            = "↲",    -- end of line
+  space          = "⋅",    -- a single <space>
+  leadmultispace = "⋅⋅⋅•", -- used cyclically to show multiple consecutive leading spaces; overrides "space"
+  tab            = "  ",  -- 3 chars `xyz` to show a <tab>; `z` is always used, `x` is prepended, `y` is used as many times as can fit
+}
+
