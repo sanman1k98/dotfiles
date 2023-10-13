d3e6eebd04cb6082c2851f85d1210a98fd0b5d86
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-10-13 09:11:11 -0400

    feat(wezterm): update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,13 +6,20 @@ local font = wezterm.font
 
 local conf = {
   font = font "Cascadia Code PL",
-  font_size = 13,
-  line_height = 1.3,
+  font_size = 14,
+  line_height = 1.6,
 
   use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
 
-  color_scheme = "Catppuccin Mocha",
+  window_padding = {
+    left = "40px",
+    right = "40px",
+    top = "40px",
+    bottom = "40px",
+  },
+
+  color_scheme = "dawnfox",
 }
 
 
