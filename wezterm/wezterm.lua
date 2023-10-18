f6b59683dc03d7b227b1c7e28ac713670459424c
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-10-18 08:20:00 -0400

    refactor(wezterm): use `wezterm.config_builder()`

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,27 +1,27 @@
 local wezterm = require "wezterm"
+local config = wezterm.config_builder()
 
-local font = wezterm.font
+config.font = wezterm.font "Cascadia Code PL"
+config.font_size = 14
+config.line_height = 1.6
 
+config.initial_cols = 120
+config.initial_rows = 36
 
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 
-local conf = {
-  font = font "Cascadia Code PL",
-  font_size = 14,
-  line_height = 1.6,
-
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
-
-  window_padding = {
-    left = "40px",
-    right = "40px",
-    top = "40px",
-    bottom = "40px",
-  },
-
-  color_scheme = "dawnfox",
+config.window_padding = {
+  left = "40px",
+  right = "40px",
+  top = "40px",
+  bottom = "40px",
 }
 
+if wezterm.gui.get_appearance():find("Dark") then
+  config.color_scheme = "carbonfox"
+else
+  config.color_scheme = "dawnfox"
+end
 
-
-return conf
+return config
