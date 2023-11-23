73a3d1dd5a8301d300cc576500fd4362eaa1327b
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-11-22 20:48:46 -0500

    refactor(zsh): add `.local/bin` to path

--- zsh/init/path.zsh
+++ zsh/init/path.zsh
@@ -1,3 +1,6 @@
+# user-specific executable files according to the XDG Base Directories specification
+path+="${HOME}/.local/bin"
+
 # add cargo installed binaries to $PATH
 path+="${HOME}/.cargo/bin"
 
