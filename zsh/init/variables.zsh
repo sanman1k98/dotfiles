cd79f00a7e8537c2bc1fc2022df952765a7a560f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-03 08:14:59 -0500

    refactor(zsh): move export to `variables.zsh`

--- zsh/init/variables.zsh
+++ zsh/init/variables.zsh
@@ -3,6 +3,8 @@ export EDITOR='nvim'
 export VISUAL='nvim'
 export MANPAGER='nvim +Man!'
 
+export HOMEBREW_PREFIX="$(brew --prefix)"
+
 # lazygit config
 export LG_CONFIG_FILE="${XDG_CONFIG_HOME}/lazygit/config.yml"
 
