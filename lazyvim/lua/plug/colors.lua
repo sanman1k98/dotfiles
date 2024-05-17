72c04d9c1377df4ea95275d6bef2bc3b40396ba7
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-05-17 11:32:49 -0400

    feat(lazyvim): docstring comment styles

--- lazyvim/lua/plug/colors.lua
+++ lazyvim/lua/plug/colors.lua
@@ -1,11 +1,26 @@
 return {
 
-	{
-		"EdenEast/nightfox.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("dawnfox")
-		end,
-	},
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      options = {
+        styles = {
+          constants = "bold",
+          comments = "italic",
+        },
+      },
+      groups = {
+        all = {
+          -- Docstrings shouldn't be italicized like regular comments.
+          ["@comment.documentation"] = { style = "nocombine" },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nightfox").setup(opts)
+      vim.cmd.colorscheme("dawnfox")
+    end,
+  },
 }
