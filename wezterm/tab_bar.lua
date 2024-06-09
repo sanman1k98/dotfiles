a3f87902a4001031bc84dd4b7b671c4d1f040e9c
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-06-09 11:35:30 -0400

    feat(wezterm): simple status in tab bar

--- wezterm/tab_bar.lua
+++ wezterm/tab_bar.lua
@@ -1,8 +1,23 @@
+local wezterm = require "wezterm"
+
 local M = {}
+
+local function update_status(window)
+  local date = wezterm.strftime "%a %x %I:%M "
+
+  local status = wezterm.format {
+    { Attribute = { Intensity = "Bold" } },
+    { Text = date },
+  }
+
+  window:set_right_status(status)
+end
 
 function M.setup(config)
   config.use_fancy_tab_bar = false
   config.hide_tab_bar_if_only_one_tab = true
+  config.status_update_interval = 1000
+  wezterm.on("update-status", update_status)
 end
 
 return M
