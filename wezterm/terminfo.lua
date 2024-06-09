a19632a108220abb19283c8d278cdd6a4d791b30
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-06-09 18:29:48 -0400

    refactor(wezterm): ensure non-nil value

--- wezterm/terminfo.lua
+++ wezterm/terminfo.lua
@@ -5,9 +5,8 @@ M.TERMINFO_DIRS = wezterm.home_dir .. "/.local/share/terminfo"
 
 ---@param config config
 function M.setup(config)
-  config.set_environment_variables = {
-    TERMINFO_DIRS = M.TERMINFO_DIRS,
-  }
+  config.set_environment_variables = config.set_environment_variables or {}
+  config.set_environment_variables.TERMINFO_DIRS = M.TERMINFO_DIRS
   config.term = "wezterm"
 end
 
