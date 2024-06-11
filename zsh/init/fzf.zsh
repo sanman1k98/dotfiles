643d1faa658f70ee0b39715c75b0284dd9aa18a5
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-06-11 17:02:07 -0400

    feat(zsh): fzf shell integration

---
+++ zsh/init/fzf.zsh
@@ -0,0 +1,9 @@
+# See https://junegunn.github.io/fzf/
+
+# Default keybindings:
+# CTRL-R to paste the selected command from history onto the command-line.
+# CTRL-T to paste the selected files and directories onto the command-line.
+# ALT-C to cd into the selected directory.
+
+# Setup shell integration.
+source <(fzf --zsh)
