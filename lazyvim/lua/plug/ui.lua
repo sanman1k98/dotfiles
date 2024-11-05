8787a3699ca81ef4b0d867e5cf733038a85e0e12
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-11-05 08:59:26 -0500

    fix(lazyvim): lualine spec

--- lazyvim/lua/plug/ui.lua
+++ lazyvim/lua/plug/ui.lua
@@ -1,23 +1,25 @@
 return {
   -- lualine customizations
-  "nvim-lualine/lualine.nvim",
-  opts = {
-    options = {
-      component_separators = { left = "│", right = "│" },
-      section_separators = { left = "", right = "" },
-    },
-    sections = {
-      lualine_y = {
-        {
-          function()
-            local line = vim.fn.line('.')
-            local col = vim.fn.virtcol('.')
-            return string.format('%03d:%03d', line, col)
-          end,
-        },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separators = { left = "│", right = "│" },
+        section_separators = { left = "", right = "" },
       },
-      lualine_z = {
-        { "progress", fmt = string.upper },
+      sections = {
+        lualine_y = {
+          {
+            function()
+              local line = vim.fn.line('.')
+              local col = vim.fn.virtcol('.')
+              return string.format('%03d:%03d', line, col)
+            end,
+          },
+        },
+        lualine_z = {
+          { "progress", fmt = string.upper },
+        },
       },
     },
   },
