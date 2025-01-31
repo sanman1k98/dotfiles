4d25da7bed7f261d46466a5219e2a449fd10d90b
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-01-31 17:04:19 -0500

    refactor(lazyvim): move overrides into ui module

--- lazyvim/lua/plug/ui.lua
+++ lazyvim/lua/plug/ui.lua
@@ -1,4 +1,31 @@
 return {
+  -- neo-tree customizations
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      add_blank_line_at_top = true,
+      hide_root_node = true,
+      retain_hidden_root_indent = true,
+      window = { width = 30 },
+      default_component_configs = {
+        indent = {
+          with_markers = false,
+          with_expanders = false,
+        },
+      },
+    },
+  },
+
+  -- TODO comments customizations
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      highlight = {
+        multiline = false,
+      },
+    },
+  },
+
   -- lualine customizations
   {
     "nvim-lualine/lualine.nvim",
