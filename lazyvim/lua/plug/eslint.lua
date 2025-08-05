96c7edb5cc0ac500e16184c0b3c2592742ae52e3
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-05 12:08:31 -0400

    impr(lazyvim): silence stylistic rules in editor
    Also implement a fix for enabling ESLint autofixing from a comment in
    issue number 5862 in the LazyVim GitHub repo.

--- lazyvim/lua/plug/eslint.lua
+++ lazyvim/lua/plug/eslint.lua
@@ -1,5 +1,75 @@
+local customizations = {
+  { rule = "style/*", severity = "off", fixable = true },
+  { rule = "format/*", severity = "off", fixable = true },
+  { rule = "*-indent", severity = "off", fixable = true },
+  { rule = "*-spacing", severity = "off", fixable = true },
+  { rule = "*-spaces", severity = "off", fixable = true },
+  { rule = "*-order", severity = "off", fixable = true },
+  { rule = "*-dangle", severity = "off", fixable = true },
+  { rule = "*-newline", severity = "off", fixable = true },
+  { rule = "*quotes", severity = "off", fixable = true },
+  { rule = "*semi", severity = "off", fixable = true },
+}
+
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        eslint = {
+          settings = {
+            rulesCustomizations = customizations,
+          },
+        },
+      },
+      setup = {
+        eslint = function()
+          local function get_client(buf)
+            return LazyVim.lsp.get_clients({ name = "eslint", bufnr = buf })[1]
+          end
 
+          local formatter = LazyVim.lsp.formatter({
+            name = "eslint: lsp",
+            primary = false,
+            priority = 200,
+            filter = "eslint",
+          })
+
+          -- Use EslintFixAll on Neovim < 0.10.0
+          -- Changed from upstream: check the version explicitly instead of
+          -- looking for `vim.lsp._require`. Seems like that check stopped working
+          -- with Neovim 0.11.
+          if vim.fn.has("nvim-0.10") == 0 then
+            formatter.name = "eslint: EslintFixAll"
+            formatter.sources = function(buf)
+              local client = get_client(buf)
+              return client and { "eslint" } or {}
+            end
+            formatter.format = function(buf)
+              local client = get_client(buf)
+              if client then
+                local pull_diagnostics = vim.diagnostic.get(
+                  buf,
+                  { namespace = vim.lsp.diagnostic.get_namespace(client.id, false) }
+                )
+                -- Older versions of the ESLint language server send push
+                -- diagnostics rather than using pull. We support both for
+                -- backwards compatibility.
+                local push_diagnostics = vim.diagnostic.get(
+                  buf,
+                  { namespace = vim.lsp.diagnostic.get_namespace(client.id, true) }
+                )
+                if (#pull_diagnostics + #push_diagnostics) > 0 then
+                  vim.cmd("EslintFixAll")
+                end
+              end
+            end
+          end
+
+          -- register the formatter with LazyVim
+          LazyVim.format.register(formatter)
+        end,
+      },
+    },
   },
 }
