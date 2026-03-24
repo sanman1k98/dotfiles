7c2cac1b1613a876214c587f6d2be7ebb3e0c749
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2026-03-24 11:32:04 -0400

    chore(zsh): remove config for pfetch
    Its been archived and I haven't had it installed in a while.

--- zsh/init/variables.zsh
+++ zsh/init/variables.zsh
@@ -8,15 +8,5 @@ export HOMEBREW_PREFIX="$(brew --prefix)"
 # lazygit config
 export LG_CONFIG_FILE="${XDG_CONFIG_HOME}/lazygit/config.yml"
 
-# pfetch: a pretty system information tool
-#
-# Which information to display.
-# Note: If 'ascii' will be used, it must come first.
-# Valid: space separated string
-#
-# DEFAULT: ascii title os host kernel uptime pkgs memory
-# OFF by default: shell editor wm de palette
-export PF_INFO="ascii title os host kernel uptime pkgs memory palette"
-
 # bat theme
 export BAT_THEME="${CONFIG_COLORS}"
